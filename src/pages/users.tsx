3066a5b4f4887875f6e48f92d65a17f54a627eca
lisear <lisearantes@gmail.com>
2022-01-05 11:53:23 -0300

    Home, Users, Profile, Jobs and Settings page created

---
+++ src/pages/users.tsx
@@ -0,0 +1,17 @@
+import Table from '../components/Table'
+
+const people = [
+  { name: 'Jane Cooper', title: 'Regional Paradigm Technician', role: 'Admin', email: 'jane.cooper@example.com' },
+  // More people...
+]
+
+export default function Users() {
+
+  const headerLabels = ['Name', 'Title', 'Email', 'role'];
+  return (
+    <Table
+      data={people}
+      headerLabels={headerLabels}
+    />
+  )
+}
